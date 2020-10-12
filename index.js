8064891b62403417a7d8a58c55d961743e88d714
Toshihiro Shinmachi <toshihi0717@gmail.com>
2020-10-12 17:19:10 +0900

    お問い合わせフォームにcss適用

--- index.js
+++ index.js
@@ -7,44 +7,6 @@ $(function () {
       $(this).next().slideToggle();
       $(this).toggleClass("no-border");
       });
-
-      jQuery("my-form").submit(function (event) {
-        event.preventDefault();
-      
-        let baseUrl = "https://docs.google.com/forms/u/0/d/e/xxxxxxxxx/formResponse";
-        let name = jQuery("[name=\"entry.xxxxxxxxx\"]").val();
-        let mail = jQuery("[name=\"entry.xxxxxxxxx\"]").val();
-        let content = jQuery("[name=\"entry.xxxxxxxxx\"]").val();
-        let radio = jQuery("[name=\"entry.xxxxxxxxx\"]:checked").val();
-        /* チェックボックスは数分だけ作る */
-        let checks = ["", "", "", ""];
-        let count = 0;
-        jQuery("[name=\"entry.xxxxxxxxx\"]:checked").each(function() {
-          checks[count] = jQuery(this).val();
-          count++;
-        });
-        let select = jQuery("[name=\"entry.xxxxxxxxx\"]").val();
-      
-        jQuery.ajax({
-            url: baseUrl,
-            data: {
-              "entry.xxxxxxxxx": name,
-              "entry.xxxxxxxxx": mail,
-              "entry.xxxxxxxxx": content,
-              "entry.xxxxxxxxx": radio,
-              "entry.xxxxxxxxx": checks[0],
-              "entry.xxxxxxxxx": checks[1],
-              "entry.xxxxxxxxx": checks[2],
-              "entry.xxxxxxxxx": checks[3],
-              "entry.xxxxxxxxx": select,
-            },
-            type: "POST",
-            dataType: "xml",
-        }).always((jqXHR, textStatus, errorThrown) => {
-          window.location.href = "thsnks.html";
-        });
-        return false;
-      });
 });
 
 $('#form').submit(function (event) {
