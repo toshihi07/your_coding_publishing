5e07daacf2057970f4a575af1b53a4597c48de19
Toshihiro Shinmachi <toshihi0717@gmail.com>
2020-10-13 14:19:32 +0900

    スクロール実装

--- index.js
+++ index.js
@@ -73,3 +73,15 @@ var Swiper = new Swiper('.swiper-container', {
     }
   }
 })
+
+$(function(){
+  var headerHight = 87;
+  // #で始まるアンカーをクリックした場合に処理
+  $('a[href^="#"]').click(function(){
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top-headerHight; //ヘッダの高さ分位置をずらす
+    $("html, body").animate({scrollTop:position}, 550, "swing");
+    return false;
+});
+});
